cf67aec9c7c02f07323535353e23840ada046898
Audra Rusert <112715202+ruserta@users.noreply.github.com>
2022-09-14 15:48:26 +0000

    58t not done :)

--- src/Prog58b.java
+++ src/Prog58b.java
@@ -2,21 +2,22 @@ import java.util.*;
 
 public class Prog58b {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
       try (Scanner input = new Scanner(System.in)) {
       System.out.print("Enter a, b, c : ");
       double a = input.nextDouble();
-      double b = inpit.nextDouble();
+      double b = input.nextDouble();
       double c = input.nextDouble();
 
       double discriminant = Math.pow(b, 2) - 4 * a * c; 
 
-      double 1root = (-b + Math.pow(discriminat, 0.5)) / (2* a);
-      double 2root = (-b + Math.pow(discriminat,0.5)) / (2 * a);
+      double root1 = (-b + Math.pow(discriminant, 0.5)) / (2* a);
+      double root2 = (-b + Math.pow(discriminant,0.5)) / (2 * a);
 
 
       System.out.println();
-      System.out.println("The roots are : " + 1root + ", " + 2root);
+      System.out.println("The roots are : " + root1 + ", " + root2);
 
       } 
-    } 
+    }
+}
---
+++ src/Prog58t.java
@@ -0,0 +1,13 @@
+//not done 
+import java.util.*;
+public class Prog58t {
+  public static void main(String[] args){
+  Scanner input = new Scanner(System.in);
+
+  System.out.print("Enter purchase price : ");
+    double purpric = input.nextDouble();
+  System.out.println("Enter amount of money paid : "); 
+    double mongiv = input.nextDouble();
+  
+  
+    
