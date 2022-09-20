e44af70e4217835999a8cf8d3f6531324d0d04a9
Aud Rusert <112715202+ruserta@users.noreply.github.com>
2022-09-20 15:06:49 +0000

    Prog58H Done

---
+++ src/Prog115a.java
@@ -0,0 +1,9 @@
+public class Prog115a {
+  public static void main(String [] args){
+    int lcv = 0; 
+  while (lcv < 36) { 
+    lcv += 2;  
+    System.out.println(lcv);
+    } 
+  } 
+} 
--- src/Prog58h.java
+++ src/Prog58h.java
@@ -1,2 +1,25 @@
 import java.util.*;
 
+public class Prog58h {
+  public static void main(String[] args) { 
+
+  try (Scanner input = new Scanner(System.in)){
+      System.out.print("Enter amount saved: ");
+      double amSav = input.nextDouble();
+      System.out.print("Enter the interest rate: "); 
+      double interest = input.nextDouble();
+      System.out.print("Enter the times compounded per year : ");
+      double tCpY = input.nextDouble();
+      System.out.print("Enter the number of days at interest: ");
+      double numDAI = input.nextDouble();
+
+      double interestEarned = amSav * (Math.pow((1+ (interest / 100 / tCpY), (tCpY * numDAI / 365))-1));
+
+    double totalAmount = amSav + interestEarned;
+
+    interestEarned = Math.round();
+    System.out.println("The interest earned is : $" + interestEarned);
+    System.out.println("The total amount is : $"+ totalAmount);
+    }
+  }
+}
