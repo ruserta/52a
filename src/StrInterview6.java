10c34bbaa8305d6c500b581ac1130ae1f958b1f6
Audra Rusert <112715202+ruserta@users.noreply.github.com>
2022-09-28 15:44:50 +0000

    lol just stuff we did in class

---
+++ src/StrInterview6.java
@@ -0,0 +1,21 @@
+// not done 
+import java.util.Scanner;
+public class StrInterview6 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("enter a string: ");
+    String myText = input.nextLine();
+
+
+    int Acount = 0;
+    int Bcount = 0;
+    int Ccount = 0; 
+    
+    for (int lcv = 0; lcv < myText.length(); lcv++){
+      char cur = myText.charAt(lcv);
+      
+    }
+    
+  }
+}
---
+++ src/StrInterview7.java
@@ -0,0 +1,23 @@
+//done 
+import java.util.Scanner;
+
+public class StrInterview7 {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.print("Enter a String: ");
+    String myText = keyboard.nextLine();
+
+    int vowelCount = 0;
+    for (int lcv = 0; lcv < myText.length(); lcv++) {
+      char cur = myText.charAt(lcv);
+      if (cur == 'a' || cur == 'e' || cur == 'i' || cur == 'o' || cur == 'u')
+        vowelCount++;
+
+    }
+
+    int conscount = myText.length() - vowelCount;
+    System.out.printf(" # Vowels : %d\t\t# Consonants: %d\n", vowelCount, conscount);
+
+  }
+}
