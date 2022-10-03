f09e89e579c9a45791983aacd42358953d27271d
Audra Rusert <112715202+ruserta@users.noreply.github.com>
2022-10-03 15:20:43 +0000

    stuff in class

---
+++ src/dfile.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class dfile {
+  public static void main(String[] args) {
+    try {
+      Scanner input = new Scanner(new File ("../LangDat/Prog285.dat"));
+      while (input.hasNext()) {
+        int id = input.nextInt();
+        int c = input.nextInt();
+        double s = input.nextDouble();
+        double com = 0;
+
+        if (c == 5 || c == 8) {
+          if (s <=5000)
+            com = s * 0.075;
+         else 
+            com = 500 * 0.075 + (s - 5000) * 0.085;
+        }else if(c == 17) {
+          if (s <= 3500)
+          com = s * 0.095;
+          else 
+            com = 3500 * 0.095 + (s-3500) * 0.12;
+          
+        }
+
+        System.out.printf("%d\t\t%d\t\t%.2f\t\t%.2f\n",id ,c,s,com);
+        
+          }
+      
+    } catch (IOException e) {
+        System.out.println("Can't find the data file!");
+      
+
+    }
+  }
+}
